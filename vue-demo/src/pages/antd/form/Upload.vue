794f0f2da1f96d0a907874586e24725d9e4be8b1
eminoda <245978782@qq.com>
2020-06-14 22:50:30 +0800

    feat #

--- vue-demo/src/pages/antd/form/Upload.vue
+++ vue-demo/src/pages/antd/form/Upload.vue
@@ -1,13 +1,101 @@
 <template>
-  <div>
+  <a-card title="图片上传">
     <h2>自定义图片上传方法</h2>
-    <h2>图片回显</h2>
-  </div>
+    <a-form :form="form" v-bind="fieldsLayout">
+      <a-form-item
+        label="约定（默认）字段"
+        extra="映射 {name:'文件名',url:'图片地址'}"
+      >
+        <pic-upload v-decorator="uploadDecorator.banner" />
+      </a-form-item>
+      <a-form-item
+        label="只含 url 字段"
+        extra="映射 {name:'以 url 代替',url:'bannerUrl'}"
+      >
+        <pic-upload
+          v-decorator="uploadDecorator.banner1"
+          :fieldMapping="{ name: '', url: 'bannerUrl' }"
+        />
+      </a-form-item>
+      <a-form-item
+        label="自定义字段"
+        extra="映射 {name:'toutiaoName',url:'toutiaoUrl'}"
+      >
+        <pic-upload
+          v-decorator="uploadDecorator.banner2"
+          :fieldMapping="{ name: 'toutiaoName', url: 'toutiaoUrl' }"
+        />
+      </a-form-item>
+      <a-form-item :label="'提交'">
+        <a-button @click="submit">提交</a-button>
+      </a-form-item>
+    </a-form>
+  </a-card>
 </template>
 
 <script>
-export default {
+import PicUpload from './modules/PicUpload'
 
+export default {
+  components: { PicUpload },
+  data() {
+    return {
+      fieldsLayout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 10 },
+      },
+      form: this.$form.createForm(this, {}),
+      uploadDecorator: {
+        banner: ['banner', {
+          rules: [{
+            required: true,
+            message: 'error'
+          }],
+          initialValue: null
+        }],
+        banner1: ['banner1', {
+          rules: [{
+            required: true,
+            message: 'error'
+          }],
+          initialValue: ''
+        }],
+        banner2: ['banner2', {
+          rules: [{
+            required: true,
+            message: 'error'
+          }],
+          initialValue: null
+        }]
+      }
+    }
+  },
+  created() {
+    const self = this
+    setTimeout(() => {
+      self.form.setFieldsValue({
+        banner: {
+          name: 'antd 上传图片示例',
+          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+        }
+      })
+      self.form.setFieldsValue({
+        banner1: 'http://www.eminoda.com/assets/footer/webcatcode.jpg'
+      })
+      self.form.setFieldsValue({
+        banner2: {
+          toutiaoName: '头条号-前端雨爸',
+          toutiaoUrl: 'http://m.eminoda.com/img/toutiao.f82d4af0.jpeg'
+        }
+      })
+
+    }, 1000)
+  },
+  methods: {
+    submit() {
+      console.log(this.form.getFieldsValue())
+    }
+  }
 }
 </script>
 
---
+++ vue-demo/src/pages/antd/form/modules/PicUpload.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <a-upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      list-type="picture-card"
+      :file-list="fileList"
+      @change="handleChange"
+    >
+      <div>
+        <a-icon type="plus" />
+        <div class="ant-upload-text">
+          Upload
+        </div>
+      </div>
+    </a-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: [Object, String],
+    fieldMapping: {
+      type: Object,
+      default() {
+        return {
+          name: 'name',
+          url: 'url'
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      isMutiple: false
+    }
+  },
+  computed: {
+    fileList() {
+      if (this.value) {
+        if (Array.isArray(this.value)) {
+          const list = []
+          for (let item of this.value) {
+            if (item.url) {
+              list.push(this.buildPreviewPic(item))
+            }
+          }
+          return list
+        } else if (this.value) {
+          return [this.buildPreviewPic(this.value)]
+        }
+      }
+      return []
+    }
+  },
+  methods: {
+    getBase64(file) {
+      if (!file) {
+        return Promise.resolve();
+      }
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
+    },
+    buildPreviewPic(item) {
+      if (typeof item == 'string') {
+        return {
+          uid: String(Date.now()),
+          name: item,
+          status: 'done',
+          url: item
+        }
+      }
+      return {
+        uid: item.uid || String(Date.now()),
+        name: item[this.fieldMapping.name] || item[this.fieldMapping.url],
+        status: 'done',
+        url: item[this.fieldMapping.url]
+      }
+    },
+    async handleChange({ file, fileList }) {
+      this.isMutiple = Array.isArray(this.value);
+      if (!this.isMutiple) {
+        if (file.status == 'remove') {
+          this.$emit('change', { [this.fieldMapping.name]: '', [this.fieldMapping.url]: '' })
+          return
+        }
+        const base64 = await this.getBase64(file.originFileObj)
+        this.$emit('change', { [this.fieldMapping.name]: file.name, [this.fieldMapping.url]: base64 })
+      } else {
+
+        const list = []
+        for (let item of fileList) {
+          const base64 = await this.getBase64(item.originFileObj)
+          list.push({ [this.fieldMapping.name]: item.name, [this.fieldMapping.url]: base64 })
+        }
+        this.$emit('change', list)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
